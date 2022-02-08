7226d47374f4857e0f6ac214fdad8e169ef524dc
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-08 00:44:57 +0000

    Created head function, and tested it with assertEqual

---
+++ head.js
@@ -0,0 +1,18 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
