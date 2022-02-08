b73866113b0bd24f5c95edc5c5c3ad4d598fc6a4
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-08 00:23:19 +0000

    created own assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TEST
+
+assertEqual(false, 0);
+assertEqual(12, 12);
