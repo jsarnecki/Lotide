513ac520d3dec203048f5cf96af7e5747280f9c3
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-09 23:29:58 +0000

    First commit, added pseudo code

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const countLetters = function(str) {
+  //Count the letters in the string
+
+  //Split the string into array
+
+  //create empty obj
+
+  //Loop thru the arr, creating new keys for obj based on letters.
+  //if they repeat, that letter key++
+
+  //return obj
+}
+
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
