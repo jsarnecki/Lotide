3c83b6b9010b25afbdbfddd7244c9d1c0d75b5b1
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-10 04:31:51 +0000

    tests work, and can not compare objs efficiently with values that have multiple arrays

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,38 @@
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObject = function(obj1, obj2) {
-  //Create keyArr
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    //console.log(`🛑 False: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+     // console.log(`🛑 False: ${arr1} !== ${arr2}`);
+      return false;
+    }
+  }
+  //console.log(`✅ Passed: ${arr1} === ${arr2}`);
+  return true;
+};
+
+const eqObjects = function(obj1, obj2) {
   const keyArr = Object.keys(obj1);
-  //Loop thru keyArr, returning false if a key doesn't exist in other obj
-  for (let elm of keyArr) {
-    for (let key in obj2) {
-      if (elm === key) {
-        if (obj1[key] !== obj2[key]) {
-          //If true, check further, compare the values of both those objs, false if no
-          console.log(`🛑 False: ${obj1[elm]} !== ${obj2[key]}`);
-          return false;
+  //Nest loop, keyArr on outside checking elm by elm looping thru obj2 keys
+  for (let key1 of keyArr) {
+    for (let key2 in obj2) {
+      if (key1 === key2) {
+        if (Array.isArray(obj2[key2])) {
+          if (!eqArrays(obj1[key1], obj2[key2])) {
+            //If the eqArr returns true, it will continue on the loop
+            return false;
+          }
+        } else if (obj1[key2] !== obj2[key2]) {
+            //console.log(`🛑 False: ${obj1[key1]} !== ${obj2[key2]}`);
+            return false;
         }
       }
     }
   }
   console.log(`✅ Passed: ${obj1} === ${obj2}`);
   return true;
-  //if loop is successful, return true
 };
 
 let assertEqual = function(actual, expected) {
@@ -28,18 +43,29 @@ let assertEqual = function(actual, expected) {
   }
 };
 
+// //TEST CODE - COMPLEX
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log("cd/dc:", assertEqual(eqObjects(cd, dc), true)); // => true
+
+const ab = {a: 5, b: [1, "fun", "4"], c: "string", d: [5, 4]};
+const ba = {c: "string", b: [1, "fun", "4"], a: 5, d: [4, 5]};
+console.log("ab/ba:", assertEqual(eqObjects(ab, ba), false)); //
+
+const cd2 = { c: "1", d: ["2", 3, 4]};
+console.log("cd/dc:", assertEqual(eqObjects(cd, cd2), false)); // => false
+
 
 //TEST CODE - SIMPLE
-let abc = {a: 1, b: 2, c: "LHL"};
-let def = {b: 2, a: 1, c: "LHL"};
-let result1 = eqObject(abc, def);
-console.log(assertEqual(result1, true)); //True
 
-let ghi = {a: 3, b: 4, c: "yo"};
-let jkl = {a: 4, b: 5, c: "yo"};
+// let abc = {a: 1, b: 2, c: "LHL"};
+// let def = {b: 2, a: 1, c: "LHL"};
+// let result1 = eqObjects(abc, def);
+// console.log(assertEqual(result1, true)); //True
 
-let result2 = eqObject(ghi, jkl);
-console.log(assertEqual(result2, false)); //False
+// let ghi = {a: 3, b: 4, c: "yo"};
+// let jkl = {a: 4, b: 5, c: "yo"};
 
-//TEST CODE - COMPLEX
-
+// let result2 = eqObjects(ghi, jkl);
+// console.log(assertEqual(result2, false)); //False
