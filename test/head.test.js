0f2fd8dd69b0fc364f7f9b593a90449d5219fd0a
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-14 23:41:48 +0000

    Implemented mocha and chai, testing the new syntax

--- test/head.test.js
+++ test/head.test.js
@@ -1,6 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([12, 36, 89]), 89);
+describe("#head", function() {
+
+  it('returns 5 for [5, 6, 7]', function() {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', function() {
+    assert(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns 12 for [12, 36, 89]', function() {
+    assert(head([12, 36, 89]), 12);
+  });
+
+});
