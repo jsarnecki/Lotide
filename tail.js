f10e76145c4dc2122f31e27d67caa57a0fa0c85e
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-14 21:19:26 +0000

    Removed manual testing

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//TAIL
-let tail = function(arr) {
+const tail = function(arr) {
   return arr.slice(1, arr.length);
 };
 
-let list = [1, 2, 3, 4, 5];
-console.log(tail(list));
-console.log(assertEqual(list.length, 5));
-
-let arrResult = tail(list);
-console.log(assertEqual(arrResult[0], 2));
-console.log(assertEqual(arrResult[1], 3));
+module.exports = tail;
