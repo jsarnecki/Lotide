84762aea6555a20245ab703b7a2c55f8d7d45822
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-14 21:25:03 +0000

    Created eqArrays test file using exports and require syntax

---
+++ test/eqArrays.test.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([null, true, false, "", "143"], [null, true, false, "", "143"]), true);
+assertEqual(eqArrays([true, false, "", "143"], [null, false, true, "", "143"]), false);
