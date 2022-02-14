29be541f48e86f10eb29074ca3f497997e7c7ef0
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-14 21:25:42 +0000

    Removed manual testing, and exported to test file

--- eqArrays.js
+++ eqArrays.js
@@ -10,18 +10,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([null, true, false, "", "69"], [null, true, false, "", "69"]), true);
-assertEqual(eqArrays([true, false, "", "69"], [null, false, true, "", "69"]), false);
+module.exports = eqArrays;
