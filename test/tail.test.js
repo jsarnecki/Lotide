3404d1f5e9df2c4fe17b7944db4939e209c5ffd7
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-15 00:03:45 +0000

    Added mocha and chai testing

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,12 +1,28 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assertDeepEqual = require('chai').assert.deepEqual;
 
-let list = [1, 2, 3, 4, 5];
+const list = [1, 2, 3, 4, 5];
 
-console.log(assertEqual(list.length, 5));
+describe("Testing for tail", function() {
 
-let arrResult = tail(list);
-console.log(assertEqual(arrResult[0], 2)); //Pass
-console.log(assertEqual(arrResult[1], 3)); //Pass
+  it('Returns [2, 3, 4] when given [1, 2, 3, 4]', function() {
 
-console.log(assertEqual(arrResult[1], 2)); //Fail
+    assertDeepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+
+  });
+
+  it('returns [2, 3, 4, 5] when given [1, 2, 3, 4, 5]', function() {
+
+    assertDeepEqual(tail(list), [2, 3, 4, 5]);
+
+  });
+
+  it('returns ["Romans", "Countrymen"] when given ["Friends", "Romans", "Countrymen"]', function() {
+
+    const actual = ["Friends", "Romans", "Countrymen"];
+    const expected = ["Romans", "Countrymen"];
+    assertDeepEqual(tail(actual), expected);
+
+  });
+
+});
