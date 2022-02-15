fe9c9edfaa487775a6a2fefca68ee19316cb45de
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-15 04:12:25 +0000

    removed manual testing, created separate testing file

--- letterPositions.js
+++ letterPositions.js
@@ -14,28 +14,5 @@ const letterPositions = function(str) {
   return result;
 };
 
+module.exports = letterPositions;
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log(`🛑Failed: ${arr1} !== ${arr2}\n(Not same length)`);
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑Failed: ${arr1} !== ${arr2}\n(Elements at index ${i} don't match)`);
-      return false;
-    }
-  }
-  console.log(`✅Passed: ${arr1} === ${arr2}`);
-  return true;
-};
-
-
-//TEST CODE
-let result2 = letterPositions("this is an excellent test sentence");
-console.log(assertArraysEqual(result2["t"], [0, 19, 21, 24, 29]));
-console.log(assertArraysEqual(result2["s"], [3, 6, 23, 26]));
-console.log(assertArraysEqual(result2["e"], [11, 14, 17, 22, 27, 30, 33]));
-
-let result1 = letterPositions("helollelllelloo");
-console.log(assertArraysEqual(result1["l"], [2, 4, 5, 7, 8, 9, 11, 12]));
